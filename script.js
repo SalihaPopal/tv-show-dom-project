b374069083d675a64e56814262248efadc9c38b2
Saliha Popal <salihapopal@gmail.com>
2023-05-02 23:30:10 +0100

    done.
    Needs some changes.

--- script.js
+++ script.js
@@ -1,19 +1,38 @@
-//You can edit ALL of the code here
-const node = document.createElement("li");
-const nodeText = document.createTextNode("episode's name");
-node.appendChild(nodeText);
-document.getElementById("root").appendChild(node);
-
 function setup() {
   const allEpisodes = getAllEpisodes();
   makePageForEpisodes(allEpisodes);
- 
-  
 }
 
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
-  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+  rootElem.classList.add("grid-container");
+
+  episodeList.forEach((episode) => {
+    const episodeElem = document.createElement("div");
+    episodeElem.classList.add("grid-item");
+  
+    // set the episode code
+    const season = episode.season.toString().padStart(2, "0");
+    const number = episode.number.toString().padStart(2, "0");
+    const episodeCode = `S${season}E${number}`;
+
+    // create the episode content
+    const nameElem = `<h2>${episode.name} - ${episodeCode}</h2>`;
+
+    const imageElem = `<img src="${episode.image.medium}" alt="${episode.name}" />`;
+    
+
+    const summaryElem = `<p>${episode.summary}</p>`;
+
+    // const linkElem = `<a href="${episode.url}">View on TVMaze.com</a>`;
+    // episodeElem.innerHTML = `${nameElem}${imageElem}${summaryElem}${linkElem}`;
+
+    // add the content to the episode element
+    episodeElem.innerHTML = `${nameElem}${imageElem}${summaryElem}`;
+
+    // add the episode element to the root element
+    rootElem.appendChild(episodeElem);
+  });
 }
 
 window.onload = setup;
